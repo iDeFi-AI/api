4306db78c997f61ad446e4060d7ea33ef7ed561f
k3y10 <k3yt3n@gmail.com>
2024-06-03 21:20:46 -0600

    attempting fix 2

--- api/index.py
+++ api/index.py
@@ -15,7 +15,7 @@ import requests
 
 load_dotenv()
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "https://idefi-ai-api.vercel.app"}})
+CORS(app)
 
 # Set up logging
 logging.basicConfig(level=logging.DEBUG)
